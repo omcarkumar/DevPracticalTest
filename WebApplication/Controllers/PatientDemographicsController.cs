29e35c40878953b795e6483c2945e98be10abb5e
Muthukumar swamy <muthukumars@jumpstart.com>
2019-04-12 17:17:25 +0530

    call web api

--- WebApplication/Controllers/PatientDemographicsController.cs
+++ WebApplication/Controllers/PatientDemographicsController.cs
@@ -3,29 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class PatientDemographicsController : Controller
     {
-        public IActionResult Index()
+        private IRestClient _restClient;
+        private string URL_PATH = "";
+            
+        public PatientDemographicsController(IRestClient restClient)
         {
+            _restClient = restClient;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var request = new RestRequest(URL_PATH + "/" + id , Method.GET);
+            request.AddUrlSegment("Id", id);
+            var response = _restClient.Execute<PatientDemographics>(request);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+            
+            return View(response);
+        }
+
+        public IActionResult Save([FromBody] PatientDemographics viewModel)
+        {
+            var request = new RestRequest(URL_PATH, Method.POST);
+            var response = _restClient.Execute<PatientDemographics>(request);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+
             return View();
         }
 
-        public IActionResult Save()
+        public IActionResult Update(int id,[FromBody] PatientDemographics viewModel)
         {
-            return View();
-        }
-
-        public IActionResult Update()
-        {
+            var request = new RestRequest(URL_PATH + "/" + id, Method.PUT);
+            request.AddUrlSegment("Id", id);
+            var response = _restClient.Execute<PatientDemographics>(request);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
             return View();
         }
 
         public IActionResult List()
         {
-            return View();
+            var request = new RestRequest(URL_PATH, Method.GET);
+            var response = _restClient.Execute<List<PatientDemographics>>(request);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+            return View(response);
         }
     }
 }
